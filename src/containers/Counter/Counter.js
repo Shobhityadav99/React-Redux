1306b298b504d50d27e35315adf3c60dee5dc55b
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-08 17:37:58 +0530

    2 reducers made from one

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -16,7 +16,7 @@ class Counter extends Component {
                 <CounterControl label="Add 5" clicked={this.props.onAdditionCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractionCounter}  />
                 <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={() =>this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(stdResult => (
                         <li key={stdResult.id} onClick={() =>this.props.onDeleteResult(stdResult.id)}>{stdResult.value}</li>
@@ -29,8 +29,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter,
-        storedResults: state.results
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     };
 };
 
@@ -40,7 +40,7 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
         onAdditionCounter: () => dispatch({type: actionTypes.ADDITION, value: 10}),
         onSubtractionCounter: () => dispatch({type: actionTypes.SUBTRACT, value: 15}),
-        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
         onDeleteResult: (id) => dispatch({type: actionTypes.DEL_RESULT,resultId: id}),
     };
 };
---
+++ src/store/reducers/results.js
@@ -0,0 +1,24 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    results: []
+}
+
+const reducer = (state = initialState, action) => {
+    if (action.type === actionTypes.STORE_RESULT) {
+        return {
+            ...state,
+            results: state.results.concat({id: new Date(), value: action.result})
+        }
+    }
+    if (action.type === actionTypes.DEL_RESULT) {
+        const updatedArray = state.results.filter(result => result.id !== action.resultId);
+        return {
+            ...state,
+            results: updatedArray
+        }
+    }
+    return state;
+};
+
+export default reducer;
