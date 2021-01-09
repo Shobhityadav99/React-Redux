9e5ae1b2ff7366ac6b39f4afcdda6308660bf8c0
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-10 03:18:16 +0530

    an utility function added

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,22 +1,16 @@
 import * as actionTypes from '../actions/actionsTypes';
-
+import { updateObject} from '../utility';
 const initialState = {
     results: []
 }
 
 const reducer = (state = initialState, action) => {
     if (action.type === actionTypes.STORE_RESULT) {
-        return {
-            ...state,
-            results: state.results.concat({id: new Date(), value: action.result})
-        }
+        return updateObject(state ,{results: state.results.concat({id: new Date(), value: action.result})});
     }
     if (action.type === actionTypes.DEL_RESULT) {
         const updatedArray = state.results.filter(result => result.id !== action.resultId);
-        return {
-            ...state,
-            results: updatedArray
-        }
+        return updateObject(state, {results: updatedArray})
     }
     return state;
 };
